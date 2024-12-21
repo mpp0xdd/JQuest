c425ec7a71d71dbae26a474ccffb5d911a36897c
mpp <mpp0xdd@gmail.com>
2024-12-21 12:58:21 +0900

    Pull up

--- src/jquest/spec/chara/DefaultRpgChara.java
+++ src/jquest/spec/chara/DefaultRpgChara.java
@@ -8,29 +8,4 @@ class DefaultRpgChara extends RpgCharaBase {
   public DefaultRpgChara(RpgCharaChipImage image, RpgMap rpgMap, ChipCoordinate coordinate) {
     super(image, rpgMap, coordinate);
   }
-
-  @Override
-  public void turnUp() {
-    image().switchUpward();
-  }
-
-  @Override
-  public void turnDown() {
-    image().switchDownward();
-  }
-
-  @Override
-  public void turnLeft() {
-    image().switchLeftward();
-  }
-
-  @Override
-  public void turnRight() {
-    image().switchRightward();
-  }
-
-  @Override
-  public void foot() {
-    image().foot();
-  }
 }
--- src/jquest/spec/chara/RpgCharaBase.java
+++ src/jquest/spec/chara/RpgCharaBase.java
@@ -68,6 +68,31 @@ abstract class RpgCharaBase extends RpgMapConcernChipBase implements RpgChara {
   }
 
   @Override
+  public void turnUp() {
+    image().switchUpward();
+  }
+
+  @Override
+  public void turnDown() {
+    image().switchDownward();
+  }
+
+  @Override
+  public void turnLeft() {
+    image().switchLeftward();
+  }
+
+  @Override
+  public void turnRight() {
+    image().switchRightward();
+  }
+
+  @Override
+  public void foot() {
+    image().foot();
+  }
+
+  @Override
   protected RpgCharaChipImage image() {
     return (RpgCharaChipImage) super.image();
   }
