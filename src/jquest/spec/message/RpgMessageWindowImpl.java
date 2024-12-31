f1fed447acc72eeb0aefcc4290dee2c75d1e7bc8
mpp <mpp0xdd@gmail.com>
2025-01-01 00:10:53 +0900

    Ensure that messages are automatically line-breaked

--- src/jquest/spec/message/RpgMessageWindowImpl.java
+++ src/jquest/spec/message/RpgMessageWindowImpl.java
@@ -5,8 +5,10 @@ import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
-import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Objects;
 import jglib.util.StringDrawer;
 import jquest.common.Coordinate;
@@ -74,28 +76,47 @@ class RpgMessageWindowImpl implements RpgMessageWindow {
       return;
     }
 
-    List<String> messageLines = message().lines();
-
     FontMetrics fontMetrics = g.getFontMetrics();
-
-    Rectangle2D messageRect =
-        messageLines.stream()
-            .map(line -> fontMetrics.getStringBounds(line, g))
-            .reduce(
-                (r1, r2) -> {
-                  r2.setFrame(r2.getX(), r1.getMaxY(), r2.getWidth(), r2.getHeight());
-                  return r1.createUnion(r2);
-                })
-            .orElseThrow();
-
     windowRect.grow(-5, -5);
-    messageRect.setFrame(
-        windowRect.getX(), windowRect.getY(), messageRect.getWidth(), messageRect.getHeight());
-    if (!windowRect.contains(messageRect)) {
-      throw new IllegalStateException();
+    g2.setColor(Color.WHITE);
+    StringDrawer.LEFT.draw(
+        g2, windowRect.x, windowRect.y, formatMessageLines(fontMetrics, windowRect.width));
+  }
+
+  private List<String> formatMessageLines(FontMetrics fontMetrics, int maxWidth) {
+    List<List<String>> result = new ArrayList<>();
+
+    ListIterator<String> messageLines = message().lines().listIterator(message().lines().size());
+    while (messageLines.hasPrevious()) {
+      String messageLine = messageLines.previous();
+      result.add(split(messageLine, fontMetrics, maxWidth));
     }
 
-    g2.setColor(Color.WHITE);
-    StringDrawer.LEFT.draw(g2, windowRect.x, windowRect.y, messageLines);
+    Collections.reverse(result);
+    return result.stream().flatMap(List::stream).toList();
+  }
+
+  private List<String> split(String line, FontMetrics fontMetrics, int maxWidth) {
+    if (fontMetrics.stringWidth(line) <= maxWidth) {
+      return List.of(line);
+    }
+
+    List<String> result = new ArrayList<>();
+    StringBuilder buffer = new StringBuilder();
+
+    for (String s : line.split("\\b{g}")) {
+      String split = buffer.toString();
+      buffer.append(s);
+      if (fontMetrics.stringWidth(buffer.toString()) > maxWidth) {
+        result.add(split);
+        buffer.setLength(0);
+        buffer.append(s);
+      }
+    }
+    if (!buffer.isEmpty()) {
+      result.add(buffer.toString());
+    }
+
+    return result;
   }
 }
