70a2febda23d2dd175d5ac742b00ed59e698f94c
mpp <mpp0xdd@gmail.com>
2025-01-01 11:03:23 +0900

    WIP

--- src/jquest/spec/message/RpgMessageChunks.java
+++ src/jquest/spec/message/RpgMessageChunks.java
@@ -1,25 +1,89 @@
 package jquest.spec.message;
 
+import java.awt.FontMetrics;
 import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.ListIterator;
 import jglib.util.StringDrawer;
+import jquest.common.Dimension;
 
 class RpgMessageChunks {
 
-  private final List<List<String>> messageChunks;
+  public static RpgMessageChunks chunk(
+      RpgMessage message, FontMetrics fontMetrics, Dimension dimension) {
+    int height = fontMetrics.getMaxDescent() + fontMetrics.getMaxAscent();
+    int chunkSize = dimension.height() / height;
+
+    List<List<String>> chunks = new ArrayList<>();
+    List<String> chunk = new ArrayList<>();
+    for (String line : format(message, fontMetrics, dimension.width())) {
+      if (chunk.size() < chunkSize) {
+        chunk.add(line);
+        continue;
+      }
+      chunks.add(List.copyOf(chunk));
+      chunk.clear();
+    }
+    if (!chunk.isEmpty()) {
+      chunks.add(List.copyOf(chunk));
+    }
+
+    return new RpgMessageChunks(chunks);
+  }
+
+  private static List<String> format(RpgMessage message, FontMetrics fontMetrics, int maxWidth) {
+    List<List<String>> result = new ArrayList<>();
+
+    ListIterator<String> lines = message.lines().listIterator(message.lines().size());
+    while (lines.hasPrevious()) {
+      String line = lines.previous();
+      result.add(split(line, fontMetrics, maxWidth));
+    }
+
+    Collections.reverse(result);
+    return result.stream().flatMap(List::stream).toList();
+  }
+
+  private static List<String> split(String line, FontMetrics fontMetrics, int maxWidth) {
+    if (fontMetrics.stringWidth(line) <= maxWidth) {
+      return List.of(line);
+    }
+
+    List<String> result = new ArrayList<>();
+    StringBuilder buffer = new StringBuilder();
+
+    for (String s : line.split("\\b{g}")) {
+      String split = buffer.toString();
+      buffer.append(s);
+      if (fontMetrics.stringWidth(buffer.toString()) > maxWidth) {
+        result.add(split);
+        buffer.setLength(0);
+        buffer.append(s);
+      }
+    }
+    if (!buffer.isEmpty()) {
+      result.add(buffer.toString());
+    }
+
+    return result;
+  }
+
+  private final List<List<String>> chunks;
   private int index;
 
-  public RpgMessageChunks(List<List<String>> messageChunks) {
-    this.messageChunks = messageChunks;
+  private RpgMessageChunks(List<List<String>> chunks) {
+    this.chunks = chunks;
     this.index = 0;
   }
 
   public void draw(Graphics g, int x, int y) {
-    StringDrawer.LEFT.draw(g, x, y, messageChunks.get(index));
+    StringDrawer.LEFT.draw(g, x, y, chunks.get(index));
   }
 
   public boolean hasNext() {
-    return index + 1 < messageChunks.size();
+    return index + 1 < chunks.size();
   }
 
   public void next() {
--- src/jquest/spec/message/RpgMessageWindowImpl.java
+++ src/jquest/spec/message/RpgMessageWindowImpl.java
@@ -5,10 +5,6 @@ import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.ListIterator;
 import java.util.Objects;
 import jquest.common.Coordinate;
 import jquest.common.Dimension;
@@ -79,70 +75,10 @@ class RpgMessageWindowImpl implements RpgMessageWindow {
     windowRect.grow(-5, -5);
 
     RpgMessageChunks messageChunks =
-        new RpgMessageChunks(
-            chunkMessageLines(
-                formatMessageLines(fontMetrics, windowRect.width), fontMetrics, windowRect.height));
+        RpgMessageChunks.chunk(
+            message, fontMetrics, Dimension.of(windowRect.width, windowRect.height));
 
     g2.setColor(Color.WHITE);
     messageChunks.draw(g2, windowRect.x, windowRect.y);
-  }
-
-  private List<List<String>> chunkMessageLines(
-      List<String> messageLines, FontMetrics fontMetrics, int maxHeight) {
-    int height = fontMetrics.getMaxDescent() + fontMetrics.getMaxAscent();
-    int chunkSize = maxHeight / height;
-
-    List<List<String>> result = new ArrayList<>();
-    List<String> chunk = new ArrayList<>();
-    for (String messageLine : messageLines) {
-      if (chunk.size() < chunkSize) {
-        chunk.add(messageLine);
-        continue;
-      }
-      result.add(List.copyOf(chunk));
-      chunk.clear();
-    }
-    if (!chunk.isEmpty()) {
-      result.add(List.copyOf(chunk));
-    }
-
-    return result;
-  }
-
-  private List<String> formatMessageLines(FontMetrics fontMetrics, int maxWidth) {
-    List<List<String>> result = new ArrayList<>();
-
-    ListIterator<String> messageLines = message().lines().listIterator(message().lines().size());
-    while (messageLines.hasPrevious()) {
-      String messageLine = messageLines.previous();
-      result.add(split(messageLine, fontMetrics, maxWidth));
-    }
-
-    Collections.reverse(result);
-    return result.stream().flatMap(List::stream).toList();
-  }
-
-  private List<String> split(String line, FontMetrics fontMetrics, int maxWidth) {
-    if (fontMetrics.stringWidth(line) <= maxWidth) {
-      return List.of(line);
-    }
-
-    List<String> result = new ArrayList<>();
-    StringBuilder buffer = new StringBuilder();
-
-    for (String s : line.split("\\b{g}")) {
-      String split = buffer.toString();
-      buffer.append(s);
-      if (fontMetrics.stringWidth(buffer.toString()) > maxWidth) {
-        result.add(split);
-        buffer.setLength(0);
-        buffer.append(s);
-      }
-    }
-    if (!buffer.isEmpty()) {
-      result.add(buffer.toString());
-    }
-
-    return result;
   }
 }
