56638be2bd7bb15498e9ee976dadf6a967f3eccd
mpp <mpp0xdd@gmail.com>
2025-01-02 19:33:12 +0900

    WIP

--- src/jquest/spec/message/RpgMessageString.java
+++ src/jquest/spec/message/RpgMessageString.java
@@ -1,3 +1,14 @@
 package jquest.spec.message;
 
-interface RpgMessageString {}
+import java.awt.Graphics;
+
+interface RpgMessageString {
+
+  void draw(Graphics g, int x, int y);
+
+  boolean hasNextChar();
+
+  boolean nextChar();
+
+  boolean isEmpty();
+}
