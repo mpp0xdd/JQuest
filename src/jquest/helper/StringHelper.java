be79125b2c5e7ff1e7fbfeacb3c3ab13553585d8
mpp <mpp0xdd@gmail.com>
2025-01-02 17:36:48 +0900

    WIP

--- src/jquest/helper/StringHelper.java
+++ src/jquest/helper/StringHelper.java
@@ -6,8 +6,12 @@ public final class StringHelper {
 
   private static final Pattern LINE_SEPARATOR_PATTERN = Pattern.compile("\\R");
 
-  public static boolean containsLineSeparator(String string) {
+  public static boolean containLineSeparator(String string) {
     return LINE_SEPARATOR_PATTERN.matcher(string).find();
+  }
+
+  public static boolean notContainLineSeparator(String string) {
+    return !containLineSeparator(string);
   }
 
   private StringHelper() {}
