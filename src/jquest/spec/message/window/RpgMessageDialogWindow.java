8025f8c18e687b068da14f058f2ac50c19dfcd46
mpp <mpp0xdd@gmail.com>
2025-01-04 17:37:50 +0900

    WIP

--- src/jquest/spec/message/window/RpgMessageDialogWindow.java
+++ src/jquest/spec/message/window/RpgMessageDialogWindow.java
@@ -2,5 +2,9 @@ package jquest.spec.message.window;
 
 import jglib.util.spec.Drawable;
 import jglib.util.spec.Rectangular;
+import jquest.spec.message.RpgMessage;
 
-public interface RpgMessageDialogWindow extends Rectangular, Drawable {}
+public interface RpgMessageDialogWindow extends Rectangular, Drawable {
+
+  RpgMessage message();
+}
